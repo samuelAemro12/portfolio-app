1b0af64da2f3b1b61ed4a960097253b2b9063fd4
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-07-31 02:15:34 +0300

    BugFix: mount component footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="min-h-screen">
       <Hero />
       <Projects />
+      <Footer />
     </div>
   );
 }

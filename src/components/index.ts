22ad9b5c34f563fe02a1c0f61d844ce037643fb1
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-08-05 04:33:52 +0300

    BugFix : Dead code removal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
 export { default as Hero } from './Hero';
 export { default as About } from './About';
 export { default as TechnicalSkills } from './TechnicalSkills';
-export { default as Services } from './Services';
 export { default as Projects } from './Projects';
-export { default as CaseStudies } from './CaseStudies';
 export { default as OpenSource } from './OpenSource';
 export { default as Certifications } from './Certifications';
 export { default as Testimonials } from './Testimonials';
